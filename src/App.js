fd39b47493da5929dc77bde206d0249d61acbd51
Hossen <mdmitho199911@gmail.com>
2022-04-07 00:38:15 +0600

    simple form,blur,submit,preventDefult

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import app from "./firebase.init"
 const auth = getAuth(app)
 
 function App() {
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+}
+const handlePasswordBlur = event =>{
+  console.log(event.target.value);
+}
+const handleFormSubmit = event => {
+  console.log('form submitted');
+  event.preventDefault()
+}
   return (
     <div className="App">
       <h1>Email password Authentication</h1>
 
-        <input type="text"/>
-      <input type="text" name="" id="" />
+<form onSubmit={handleFormSubmit}>
+<input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br />
+      <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+      <br />
+      <input type="submit" value="Login" />
+</form>
     </div>
   );
 }
